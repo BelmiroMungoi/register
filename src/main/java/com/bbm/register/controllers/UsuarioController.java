b48e87d687389f3e9133d3fd2f9617c9ee2348dc
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-01 15:53:05 +0200

    adicionando os metodos de listar e de editar usuario

--- src/main/java/com/bbm/register/controllers/UsuarioController.java
+++ src/main/java/com/bbm/register/controllers/UsuarioController.java
@@ -1,9 +1,14 @@
 package com.bbm.register.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.bbm.register.model.UsuarioEntity;
 import com.bbm.register.repository.UsuarioRepository;
@@ -13,15 +18,36 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastroUsuario")
 	public String init() {
 		return "cadastros/cadastroUsuario";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/salvarUsuario")
-	public String salvarUsuario(UsuarioEntity usuario) {
+	public ModelAndView salvarUsuario(UsuarioEntity usuario) {
 		usuarioRepository.save(usuario);
-		return "cadastros/cadastroUsuario";
+		// Após salvar os dados irá exibir na tela
+		ModelAndView view = new ModelAndView("cadastros/cadastroUsuario");
+		Iterable<UsuarioEntity> usuarios = usuarioRepository.findAll();
+		view.addObject("usuarios", usuarios);
+		return view;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/listarUsuario")
+	public ModelAndView listarUsuario() {
+		ModelAndView view = new ModelAndView("cadastros/cadastroUsuario");
+		Iterable<UsuarioEntity> usuarios = usuarioRepository.findAll();
+		view.addObject("usuarios", usuarios);
+		return view;
+	}
+
+	@GetMapping("/editarUsuario/{idUser}")
+	public ModelAndView editarUsuario(@PathVariable("idUser") Long idUser) {
+		Optional<UsuarioEntity> usuario = usuarioRepository.findById(idUser);
+		
+		ModelAndView view = new ModelAndView("cadastro/cadastroUsuario");
+		view.addObject("usuario", usuario.get());
+		return view;
 	}
 }
