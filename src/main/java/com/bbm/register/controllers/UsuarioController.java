e7c7bb1a271f10bea3b38781e34a6cbf55b35a14
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-02 17:31:23 +0200

    adicionando o metodo de pesquisa

--- src/main/java/com/bbm/register/controllers/UsuarioController.java
+++ src/main/java/com/bbm/register/controllers/UsuarioController.java
@@ -4,11 +4,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bbm.register.model.UsuarioEntity;
@@ -66,6 +67,16 @@ public class UsuarioController {
 		view.addObject("usuarios", usuarioRepository.findAll());
 		view.addObject("usuario", new UsuarioEntity());
 
+		return view;
+	}
+	
+	@PostMapping("**/pesquisarUsuario")
+	public ModelAndView pesquisarUsuario(@RequestParam("nomePesquisa") String nomePesquisa) {
+		
+		ModelAndView view = new ModelAndView("cadastros/cadastroUsuario");
+		view.addObject("usuarios", usuarioRepository.findByNome(nomePesquisa.trim().toUpperCase()));
+		view.addObject("usuario", new UsuarioEntity());
+		
 		return view;
 	}
 }
