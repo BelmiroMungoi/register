6902d7652d20858dd72db055541a861a57c84bec
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-02 17:33:04 +0200

    adicionando uma query para pesquisa de usuário

--- src/main/java/com/bbm/register/repository/UsuarioRepository.java
+++ src/main/java/com/bbm/register/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package com.bbm.register.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,4 +13,6 @@ import com.bbm.register.model.UsuarioEntity;
 @Transactional
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long>{
 
+	@Query("select u from UsuarioEntity u where upper(trim(u.nome)) like %?1% ")
+	List<UsuarioEntity> findByNome(String nome);
 }
