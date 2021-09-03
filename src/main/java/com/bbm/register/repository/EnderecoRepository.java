bee049ea25e9be8cb8ce5db6cee3673bacc6d6ac
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-03 13:29:51 +0200

    criando o repository de endereco

---
+++ src/main/java/com/bbm/register/repository/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.bbm.register.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bbm.register.model.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
