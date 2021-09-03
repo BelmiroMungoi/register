44c4eae0c0d0ebdaf55f887d9b97a6cdcbc913df
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-03 15:55:27 +0200

    criando a query para carregar os enderecos

--- src/main/java/com/bbm/register/repository/EnderecoRepository.java
+++ src/main/java/com/bbm/register/repository/EnderecoRepository.java
@@ -1,11 +1,18 @@
 package com.bbm.register.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bbm.register.model.Endereco;
 
 @Repository
+@Transactional
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
+	@Query("select e from Endereco e where e.usuario.id = ?1")
+	public List<Endereco> getEnderecos(Long id);
 }
