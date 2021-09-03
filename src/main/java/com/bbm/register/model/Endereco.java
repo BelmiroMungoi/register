2fac5ae74a342a6d25b4ca287db2a7915b7e506d
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-03 13:28:31 +0200

    criando a entidade endereco

---
+++ src/main/java/com/bbm/register/model/Endereco.java
@@ -0,0 +1,70 @@
+package com.bbm.register.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+
+	@Column(nullable = false)
+	private String distrito;
+
+	@Column(nullable = false)
+	private String bairo;
+
+	@Column(nullable = false)
+	private String telefone;
+
+	@ManyToOne
+	private UsuarioEntity usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDistrito() {
+		return distrito;
+	}
+
+	public void setDistrito(String distrito) {
+		this.distrito = distrito;
+	}
+
+	public String getBairo() {
+		return bairo;
+	}
+
+	public void setBairo(String bairo) {
+		this.bairo = bairo;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public UsuarioEntity getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(UsuarioEntity usuario) {
+		this.usuario = usuario;
+	}
+
+}
