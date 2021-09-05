27366c680e8e9ef8a334c6a4f589d4a802ecdf76
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-05 11:32:55 +0200

    testando a criptografacao de dados

--- src/test/java/com/bbm/register/RegisterApplicationTests.java
+++ src/test/java/com/bbm/register/RegisterApplicationTests.java
@@ -2,12 +2,16 @@ package com.bbm.register;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class RegisterApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testCripto() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String result = encoder.encode("admin");
+		System.out.println("Result = " + result);
 	}
 
 }
