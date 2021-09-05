0867a5957b19914260fda00d59d6b836b2a68b63
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-05 20:26:15 +0200

    adicionando algumas configuracoes do spring

--- src/main/java/com/bbm/register/RegisterApplication.java
+++ src/main/java/com/bbm/register/RegisterApplication.java
@@ -2,8 +2,16 @@ package com.bbm.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.bbm.register.model"})
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.bbm.register.repository"})
+@EnableTransactionManagement
 public class RegisterApplication {
 
 	public static void main(String[] args) {
