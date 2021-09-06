f254e78f48582311b593df8c965c9b49d8c1db67
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-06 17:16:28 +0200

    verificando a autoridade do usuario

--- src/main/java/com/bbm/register/service/ImplementsUserDetailsService.java
+++ src/main/java/com/bbm/register/service/ImplementsUserDetailsService.java
@@ -1,28 +1,33 @@
 package com.bbm.register.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bbm.register.model.Usuario;
 import com.bbm.register.repository.UsuarioRepository;
 
 @Service
-public class ImplementsUserDetailsService implements UserDetailsService{
+@Transactional
+public class ImplementsUserDetailsService implements UserDetailsService {
 
 	@Autowired
 	private UsuarioRepository repository;
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Usuario usuario = repository.findByUserName(username);
-		
+
 		if (usuario == null) {
 			throw new UsernameNotFoundException("Usuário não encontrado!!!");
 		}
-		return usuario;
+		return new User(usuario.getUsername(), usuario.getPassword(), usuario.isEnabled(),
+				usuario.isAccountNonExpired(), usuario.isCredentialsNonExpired(), usuario.isAccountNonLocked(),
+				usuario.getAuthorities());
 	}
 
 }
