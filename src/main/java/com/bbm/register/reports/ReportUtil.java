c155025517c7de19f5dd3b54e188d92b5f3265b7
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-11 22:28:34 +0200

    Adicionando o metodo para a criacao do relatorio

--- src/main/java/com/bbm/register/reports/ReportUtil.java
+++ src/main/java/com/bbm/register/reports/ReportUtil.java
@@ -1,11 +1,18 @@
 package com.bbm.register.reports;
 
+import java.io.File;
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 
 import org.springframework.stereotype.Component;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Component
 public class ReportUtil implements Serializable{
@@ -16,7 +23,15 @@ public class ReportUtil implements Serializable{
 	//retorna o pdf em bytes para download no navegador
 	public byte[] geraRelatorio(List listaDados, String relatorio, ServletContext context) throws Exception{
 		
+		//Cria a lista de dados para a impressao
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(listaDados);
+		 
+		//Carrega o caminho do arquivo jasper
+		String url = context.getRealPath("reports") + File.separator + relatorio + ".jasper";
 		
-		return null;
+		//Carrega o arquivo jasper passando os dados
+		JasperPrint printer = JasperFillManager.fillReport(url, new HashMap(), dataSource);
+		
+		return JasperExportManager.exportReportToPdf(printer);
 	}
 }
