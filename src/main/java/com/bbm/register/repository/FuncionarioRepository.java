8824ad8f1b180c8196629e999224783fabfa7ba8
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-18 12:50:00 +0200

    metodo para pesquisa por nome com paginacao

--- src/main/java/com/bbm/register/repository/FuncionarioRepository.java
+++ src/main/java/com/bbm/register/repository/FuncionarioRepository.java
@@ -2,6 +2,10 @@ package com.bbm.register.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -22,4 +26,20 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
 	@Query("select u from Funcionario u where u.sexo = ?1")
 	List<Funcionario> findBySexo(String sexo);
 	
+	default Page<Funcionario> findByName(String nome, Pageable pageable){
+		
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome(nome);
+		
+		//Configurando a pesquisa para consultar por partes do nome(like no SQL)
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny()
+				.withMatcher("nome", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+	
+		//Une o objecto com o valor e configura a consulta
+		Example<Funcionario> example = Example.of(funcionario, exampleMatcher);
+		
+		Page<Funcionario> funcionarios = findAll(example, pageable);
+		
+		return funcionarios;
+	}
 }
