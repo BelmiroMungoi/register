3dc82ab03c7b736bf0e07e78bd2c0c0bb47fad12
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-18 12:56:21 +0200

    adicionando o metodo de consulta por sexo com paginacao

--- src/main/java/com/bbm/register/repository/FuncionarioRepository.java
+++ src/main/java/com/bbm/register/repository/FuncionarioRepository.java
@@ -15,31 +15,50 @@ import com.bbm.register.model.Funcionario;
 
 @Repository
 @Transactional
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 
 	@Query("select u from Funcionario u where upper(trim(u.nome)) like %?1% ")
 	List<Funcionario> findByNome(String nome);
-	
+
 	@Query("select u from Funcionario u where upper(trim(u.nome)) like %?1% and sexo = ?2")
 	List<Funcionario> findByNomeSexo(String nome, String sexo);
-	
+
 	@Query("select u from Funcionario u where u.sexo = ?1")
 	List<Funcionario> findBySexo(String sexo);
-	
-	default Page<Funcionario> findByName(String nome, Pageable pageable){
-		
+
+	//Metodo para consulta de funcionarios com uma paginacao, vai trazer 5 funcionarios por vez
+	default Page<Funcionario> findByName(String nome, Pageable pageable) {
+
 		Funcionario funcionario = new Funcionario();
 		funcionario.setNome(nome);
-		
-		//Configurando a pesquisa para consultar por partes do nome(like no SQL)
-		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny()
-				.withMatcher("nome", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
-	
-		//Une o objecto com o valor e configura a consulta
+
+		// Configurando a pesquisa para consultar por partes do nome(like no SQL)
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny().withMatcher("nome",
+				ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+
+		// Une o objecto com o valor e configura a consulta
 		Example<Funcionario> example = Example.of(funcionario, exampleMatcher);
-		
+
 		Page<Funcionario> funcionarios = findAll(example, pageable);
-		
+
+		return funcionarios;
+	}
+
+	//Metodo de consulta de funcionarios por sexo com paginacao
+	default Page<Funcionario> findBySex(String sexo, Pageable pageable) {
+
+		Funcionario funcionario = new Funcionario();
+		funcionario.setSexo(sexo);
+
+		// Configurando a pesquisa para consultar por partes do nome(like no SQL)
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny().withMatcher("sexo",
+				ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+
+		// Une o objecto com o valor e configura a consulta
+		Example<Funcionario> example = Example.of(funcionario, exampleMatcher);
+
+		Page<Funcionario> funcionarios = findAll(example, pageable);
+
 		return funcionarios;
 	}
 }
